4775f6fab2389385dc4c3d9a0378f93de658d681
thuotracy <tracyjacobs775@gmail.com>
2022-10-25 21:45:32 +0300

    made changes

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,10 +9,14 @@ const Header = () => (
     <div className='app__wrapper_info'>
       <SubHeading title="Chase the new Flavour" />
       <h1 className='app__header-h1'>The Key to FINE DINING</h1>
+      <p className='p__opensans' style={{ margin: '2rem 0'}}>
+        Lorem ipsum, in graphical and textual context, refers to filler text that is placed in a document or visual presentation.
+      </p>
+      <button type='button' className='custom__button'>View Menu</button>
     </div>
 
     <div className='app__wrapper_img'>
-
+      <img src={images.welcome} alt='header img'/>
     </div>
   </div>
 );
