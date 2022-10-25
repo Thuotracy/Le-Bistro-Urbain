9b742829d83e0231b4781a8a523b35cf02494cbe
thuotracy <tracyjacobs775@gmail.com>
2022-10-25 18:40:06 +0300

    created a header section

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
-const SubHeading = () => (
-  <div>
-    SubHeading
+import { images } from "../../constants"
+
+const SubHeading = ({title}) => (
+  <div style={{ marginBottom: '1rem' }}>
+    <p className='p__cornorant'>{title}</p>
+    <img src={images.spoon} alt='spoon' className='spoon__img'/>
   </div>
 );
 
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id='home' >
+    <div className='app__wrapper_info'>
+      <SubHeading title="Chase the new Flavour" />
+      <h1 className='app__header-h1'>The Key to FINE DINING</h1>
+    </div>
+
+    <div className='app__wrapper_img'>
+
+    </div>
   </div>
 );
 
