2387eb6eb36057aec5f5df35e8eb46377c4b3fda
thuotracy <tracyjacobs775@gmail.com>
2022-10-27 08:27:08 +0300

    created the about us section

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import { images } from "../../constants"
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className='app__aboutus app__bg flex__center section__padding' id='about'>
+    <div className='app__aboutus-overlay flex__center'>
+      <img src={images.G} alt='g letter'/>
+    </div>
   </div>
 );
 
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -7,8 +7,8 @@ import './Header.css';
 const Header = () => (
   <div className='app__header app__wrapper section__padding' id='home' >
     <div className='app__wrapper_info'>
-      <SubHeading title="Chase the new Flavour" />
-      <h1 className='app__header-h1'>The Key to FINE DINING</h1>
+      <SubHeading title="" />
+      <h1 className='app__header-h1'>Choose the New FLAVOUR</h1>
       <p className='p__opensans' style={{ margin: '2rem 0'}}>
         Lorem ipsum, in graphical and textual context, refers to filler text that is placed in a document or visual presentation.
       </p>
