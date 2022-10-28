358a95dad52fb12962d3f316a6904c9d66c666a2
thuotracy <tracyjacobs775@gmail.com>
2022-10-28 16:37:17 +0300

    made changes

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -2,9 +2,24 @@ import React from 'react';
 
 import './MenuItem.css';
 
-const MenuItem = () => (
-  <div>
-    MenuItem
+const MenuItem = ({title, price, tags}) => (
+  <div className='app__menuitem'>
+    <div className='app__menuitem-head'>
+      <div className='app_menuitem-name'>
+        <p className='p__cornorant' style={{color: '#DCCA87'}}>{title}</p>
+      </div>
+
+      <div className='app__menuitem-dash'/>
+
+      <div className='app_menuitem-price'>
+        <p className='p__cornorant'>{price}</p>
+      </div>
+    </div>
+
+    {/* <div className='app__menuitems-sub'>
+      <p className='p__opensans' style={{color: '#AAA'}}>{tags}</p>
+    </div> */}
+    
   </div>
 );
 
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -16,8 +16,8 @@ const SpecialMenu = () => (
         <p className='app__specialMenu_menu_heading'>Wine and Beer</p>
         <div className='app__specialMenu_menu_items'>
           {data.wines.map((wine, index) => (
-            <p>{wine.title}</p>
-          ))}
+            <MenuItem key={wine.title + index} title={wine.title} price={wine.price} tags={wine.tags}/>
+            ))}
         </div>
       </div>
 
@@ -29,7 +29,7 @@ const SpecialMenu = () => (
         <p className='app__specialMenu_menu_heading'>Cocktails</p>
         <div className='app__specialMenu_menu_items'>
           {data.cocktails.map((cocktail, index) => (
-            <p>{cocktail.title}</p>
+            <MenuItem key={cocktail.title + index} title={cocktail.title} price={cocktail.price} tags={cocktail.tags}/>
           ))}
         </div>
       </div>
