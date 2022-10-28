16ecbfb886fde4b405cd4b423a0e11fe0705753e
thuotracy <tracyjacobs775@gmail.com>
2022-10-28 15:54:00 +0300

    added the cocktails menu

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -20,6 +20,24 @@ const SpecialMenu = () => (
           ))}
         </div>
       </div>
+
+      <div className='app__specialMenu-menu_img'>
+        <img src={images.menu} alt="menu img" />
+      </div>
+
+      <div className='app__specialMenu-menu_cocktails flex__center'>
+        <p className='app__specialMenu_menu_heading'>Cocktails</p>
+        <div className='app__specialMenu_menu_items'>
+          {data.cocktails.map((cocktail, index) => (
+            <p>{cocktail.title}</p>
+          ))}
+        </div>
+      </div>
+      
+    </div>
+
+    <div style={{marginTop: '15px'}}>
+      <button type='button'></button>
     </div>
   </div>
 );
