ad627a9f496943f7ccc9b91074f49d06be334e57
thuotracy <tracyjacobs775@gmail.com>
2022-10-28 15:54:55 +0300

    created a view more button in the drinks menu

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -37,7 +37,7 @@ const SpecialMenu = () => (
     </div>
 
     <div style={{marginTop: '15px'}}>
-      <button type='button'></button>
+      <button type='button' className='custom__button'>View More</button>
     </div>
   </div>
 );
