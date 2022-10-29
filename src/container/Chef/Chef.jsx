c508b21480cb008849f4edd55a1ee583849a5c89
thuotracy <tracyjacobs775@gmail.com>
2022-10-29 14:20:21 +0300

    created the chef section

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import { SubHeading } from '../../components'
+import { images } from '../../constants';
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper section__padding'>
+    <div className='app__wrapper_img app__wrapper_img_reverse'>
+      <img src={images.chef} alt="chef" />
+    </div>
+
+    <div className='app__wrapper_info'>
+      <SubHeading title=''/>
+      <h1 className='headtext__cornorant'>Our Belief</h1>
+    </div>
   </div>
 );
 
