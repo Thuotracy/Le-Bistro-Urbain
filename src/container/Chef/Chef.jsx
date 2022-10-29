7d232b3726cd16526b7da3d6d097844f833a81e5
thuotracy <tracyjacobs775@gmail.com>
2022-10-29 14:56:52 +0300

    styled the chef's section

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -13,6 +13,14 @@ const Chef = () => (
     <div className='app__wrapper_info'>
       <SubHeading title=''/>
       <h1 className='headtext__cornorant'>Our Belief</h1>
+
+      <div className='app__chef-content'>
+        <div className='app__chef-content_quote'>
+          <img src={images.quote} alt="quote"/>
+          <p className='p__opensans'>A good restaurant is like a vacation; it transports you, and it becomes a lot more than just about the food.</p>
+        </div>
+        <p className='p__opensans'>A great restaurant doesn't distinguish itself by how few mistakes it makes but by how well they handle those mistakes.</p>
+      </div>
     </div>
   </div>
 );
