8f4350fdd8db71818c0237871fac9ed49d0ebc40
thuotracy <tracyjacobs775@gmail.com>
2022-10-29 16:10:36 +0300

    styled the awards section

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -18,7 +18,7 @@ const Laurels = () => (
   <div className="app__bg app__wrapper section__padding" id="awards">
     <div className="app__wrapper_info">
       <SubHeading title="" />
-      <h1 className="headtext__cormorant">Our Laurels</h1>
+      <h1 className="headtext__cormorant">Our Awards</h1>
 
       <div className="app__laurels_awards">
         {data.awards.map((award) => <AwardCard award={award} key={award.title} />)}
