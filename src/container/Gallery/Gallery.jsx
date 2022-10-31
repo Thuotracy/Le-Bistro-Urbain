5c34ce1d325cbe502779e5129ee7dcb89c20d7a7
thuotracy <tracyjacobs775@gmail.com>
2022-10-31 16:33:34 +0300

    removed the scrolling bar

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BsArrowLeftShort, BsRightArrowShort } from 'react-icons/bs'
+import { BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
 
 import { SubHeading } from '../../components';
 import { images } from '../../constants';
@@ -8,27 +8,42 @@ import './Gallery.css';
 const Gallery = () => {
   const scrollRef = React.useRef(null);
 
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+
+    if (direction === 'left') {
+      current.scrollLeft -= 300;
+    } else {
+      current.scrollLeft += 300;
+    }
+  };
+
   return (
-  <div className='app__gallery flex__center'>
-    <div className='app__gallery-content'>
-      <SubHeading title="" />
-      <h1 className='headtext__cornorant'>Gallery</h1>
-      <p className='p__opensans' style={{ color: '#AAA', marginTop: '2rem'}}>
-      Lorem ipsum, in graphical and textual context, refers to filler text that is placed in a document or visual presentation. Lorem ipsum is derived from the Latin "dolorem ipsum" roughly translated as "pain itself."
-      </p>
-      <button type='button' className='custom__button'>View More</button>
-    </div>
-
-    <div className='app__gallery-images'>
-      <div className='app__gallery-images_container' ref={scrollRef}></div>
-
-      <div className='app__gallery-images_arrow'>
-        <BsArrowLeftShort className='gallery__arrow-icon' onClick={() => scroll('left')} />
-        <BsArrowRightShort className='gallery__arrow-icon' onClick={() => scroll('right')} />
+    <div className="app__gallery flex__center">
+      <div className="app__gallery-content">
+        <SubHeading title="" />
+        <h1 className="headtext__cormorant">Photo Gallery</h1>
+        <p className="p__opensans" style={{ color: '#AAAAAA', marginTop: '2rem' }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.<br></br>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu
+          </p>
+        <button type="button" className="custom__button">View More</button>
+      </div>
+      <div className="app__gallery-images">
+        <div className="app__gallery-images_container" ref={scrollRef}>
+          {[images.gallery01, images.gallery02, images.gallery03, images.gallery04].map((image, index) => (
+            <div className="app__gallery-images_card flex__center" key={`gallery_image-${index + 1}`}>
+              <img src={image} alt="gallery_image" />
+            </div>
+          ))}
+        </div>
+        <div className="app__gallery-images_arrows">
+          <BsArrowLeftShort className="gallery__arrow-icon" onClick={() => scroll('left')} />
+          <BsArrowRightShort className="gallery__arrow-icon" onClick={() => scroll('right')} />
+        </div>
       </div>
     </div>
-  </div>
   );
-}
+};
 
 export default Gallery;
