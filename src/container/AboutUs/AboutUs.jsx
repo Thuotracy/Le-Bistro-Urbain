053df0634005ff516f45e7ae5c75d2c0ad68ae33
thuotracy <tracyjacobs775@gmail.com>
2022-11-01 16:10:01 +0300

    made changes

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -6,7 +6,7 @@ import './AboutUs.css';
 const AboutUs = () => (
   <div className='app__aboutus app__bg flex__center section__padding' id='about'>
     <div className='app__aboutus-overlay flex__center'>
-      <img src={images.G} alt='g letter'/>
+      {/* <img src={images.G} alt='g letter'/> */}
     </div>
 
     <div className='app__aboutus-content flex__center'>
