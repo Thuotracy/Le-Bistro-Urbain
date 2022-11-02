6b07aa4d06a789b2738540bdb7381831c64c66b5
thuotracy <tracyjacobs775@gmail.com>
2022-11-02 16:49:49 +0300

    styled the news letter section

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 import './FooterOverlay.css';
 
 const FooterOverlay = () => (
-  <div className='app__footerOverlay'>
-    <div className='app__footerOverlay-black' />
-    <div className='app__footerOverlay-img app__bg' />
+  <div className="app__footerOverlay">
+    <div className="app__footerOverlay-black" />
+    <div className="app__footerOverlay-img app__bg" />
   </div>
 );
 
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
 import { FooterOverlay, Newsletter } from '../../components';
-
 import './Footer.css';
 
 const Footer = () => (
-  <div className='ap__footer section__padding'>
+  <div className='app__footer section__padding'>
     <FooterOverlay />
     <Newsletter />
   </div>
