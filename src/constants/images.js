b1fa5677184e31af07c0073496782d340e1e6b01
thuotracy <tracyjacobs775@gmail.com>
2023-04-26 16:39:12 +0300

    made chnages

--- src/constants/images.js
+++ src/constants/images.js
@@ -6,7 +6,7 @@ import gallery02 from '../assets/gallery02.webp';
 import gallery03 from '../assets/gallery03.png';
 import gallery04 from '../assets/gallery04.png';
 import gallery05 from '../assets/gallery05.jpg';
-import gallery06 from '../assets/gallery06.png';
+import gallery06 from '../assets/gallery06.jpg';
 import gallery07 from '../assets/gallery07.jpg';
 import gallery08 from '../assets/gallery08.jpg';
 import knife from '../assets/knife.png';
